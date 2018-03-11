e49e0e1345f8fad967502d6d3ef81d7e0f1f08a9
Bill Imus <bilimus@gmail.com>
2018-03-12 01:24:01 +0500

    изменение времени
    с 50 на 10

--- test-google.py
+++ test-google.py
@@ -14,4 +14,4 @@ def test_example(driver):
     driver.get("http://www.google.com/")
     driver.find_element_by_name("q").send_keys("webdriver")
     driver.find_element_by_name("btnK").click()
-    WebDriverWait(driver, 20).until(EC.title_is("webdriver - Google qidiruvi"))
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google qidiruvi"))
