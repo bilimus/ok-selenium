42faf04d0ec155e18265021009f58b85803350bc
Bill Imus <bilimus@gmail.com>
2018-03-21 23:08:20 +0500

    добавил комменты
    в 10 задачу

--- test-lesson10-duckcolor_Firefox.py
+++ test-lesson10-duckcolor_Firefox.py
@@ -38,14 +38,14 @@ def test_example(driver):
     pr_reg_color = box.find_element_by_css_selector('.regular-price').value_of_css_property('color')
     pr_reg_color = Color.from_string(pr_reg_color).hex
 
-    # Сравниваем равность по значению 3 цветов
+    # Сравниваем равность по значению 3 цветов, серый цвет
     assert pr_reg_color[1:3] == pr_reg_color[3:5]
     assert pr_reg_color[5:7] == pr_reg_color[3:5]
 
     pr_camp_color = box.find_element_by_css_selector('.campaign-price').value_of_css_property('color')
     pr_camp_color = Color.from_string(pr_camp_color).hex
 
-    # Тест 2 цветов на равенсто 0
+    # Тест 2 цветов на равенсто 0, красный цвет
     assert pr_camp_color[5:7] == '00'
     assert pr_camp_color[3:5] == '00'
 
@@ -66,6 +66,7 @@ def test_example(driver):
     pr_reg_price = box.find_element_by_css_selector('.regular-price').text
     pr_camp_price = box.find_element_by_css_selector('.campaign-price').text
 
+    # Переходим на страницу продукта
     box.find_element_by_tag_name("a").click()
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "box-product")))
 
@@ -75,7 +76,7 @@ def test_example(driver):
     snd_reg_price = box.find_element_by_css_selector('.regular-price').text
     snd_camp_price = box.find_element_by_css_selector('.campaign-price').text
 
-    # Проверка равенста имени продукта, обычной и спец цены
+    # Проверка равенста имени продукта, обычной и спец цены из разных страниц
     assert pr_name == snd_name
     assert pr_reg_price == snd_reg_price
     assert pr_camp_price == snd_camp_price
@@ -95,14 +96,14 @@ def test_example(driver):
     snd_reg_color = box.find_element_by_css_selector('.regular-price').value_of_css_property('color')
     snd_reg_color = Color.from_string(pr_reg_color).hex
 
-    # Сравниваем равность по значению 3 цветов
+    # Сравниваем равность по значению 3 цветов (серый цвет)
     assert snd_reg_color[1:3] == snd_reg_color[3:5]
     assert snd_reg_color[5:7] == snd_reg_color[3:5]
 
     snd_camp_color = box.find_element_by_css_selector('.campaign-price').value_of_css_property('color')
     snd_camp_color = Color.from_string(pr_camp_color).hex
 
-    # Тест 2 цветов на равенсто 0
+    # Тест 2 цветов на равенсто 0, красный цвет
     assert snd_camp_color[5:7] == '00'
     assert snd_camp_color[3:5] == '00'
 
